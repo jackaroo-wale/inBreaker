e3bf746779a024f251ff7f47d74c7e799bcf915a
Thomas Cullum <thomascullum05@gmail.com>
2024-06-05 17:06:51 +0200

    done

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,6 @@ class Member < ApplicationRecord
   belongs_to :user
   belongs_to :team
 
-  validates :weekly_points, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :total_points, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :weekly_points, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :total_points, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
