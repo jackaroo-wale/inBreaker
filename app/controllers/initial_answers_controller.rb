a2361d52123fa3afe5324dd025c37f211617cdd2
jack wale <jackwale89@gmail.com>
2024-06-06 17:45:54 +0200

    gsub is kind of working for 3 wrong answers

--- app/controllers/initial_answers_controller.rb
+++ app/controllers/initial_answers_controller.rb
@@ -11,9 +11,9 @@ class InitialAnswersController < ApplicationController
     @initial_answer.user = current_user
 
     if @initial_answer.save
-      # need to create a elsif about the final card then redirect
       @initial_answer.wrong_answers = generate_and_save_false_answers(@initial_question, @initial_answer.content)
       p "WE HAVE A PROBLEM" unless @initial_answer.save!
+
       if next_initial_question
         redirect_to initial_question_path(next_initial_question), notice: "Your answer has been saved successfully."
       else
@@ -35,9 +35,6 @@ class InitialAnswersController < ApplicationController
   end
 
   def generate_and_save_false_answers(initial_question, initial_answer)
-    # Call OpenAI service to generate false answers
-    # Save false answers along with the question and the correct answer
-
     client = OpenAI::Client.new
 
     input_message = "The question is #{initial_question}"
@@ -52,9 +49,12 @@ class InitialAnswersController < ApplicationController
       })
 
     @generated_response = chaptgpt_response["choices"][0]["message"]["content"]
+    # p chaptgpt_response
+    # p @generated_response
+    messy_answers = @generated_response.split
 
-    p chaptgpt_response
-    p @generated_response
-    @generated_response.split
+    wrong_answers_array = messy_answers.map { |answer| answer.gsub(/[^a-zA-Z]/, '') }
+
+    return wrong_answers_array
   end
 end
