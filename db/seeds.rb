92d62bca58c45c9af0b976a711c4fa3ac81b1f2d
Thomas Cullum <thomascullum05@gmail.com>
2024-06-07 16:03:18 +0200

    full seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,38 +29,188 @@ initial_question3 = InitialQuestion.create(content: "What is your favourite hobb
 initial_question4 = InitialQuestion.create(content: "What is your favourite type of book?")
 initial_question5 = InitialQuestion.create(content: "What do you think is the most important aspect for teamwork?")
 
-user1 = User.create(email: 'user1@example.com', password: 'password')
-user2 = User.create(email: 'user2@example.com', password: 'password')
+user1 = User.create(email: 'jack@example.com', password: 'password', username: "JayCee", description: "Cowboy in a puffer jacket")
+file = URI.open("https://m.media-amazon.com/images/I/61ktJwNsInL._AC_SL1500_.jpghttps://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jphttps://m.media-amazon.com/images/I/61ktJwNsInL._AC_SL1500_.jpg")
+user1.profile_image.attach(io: file, filename: "cowboy.png", content_type: "image/png")
+user1.save
+
+
+user2 = User.create(email: 'prince2@example.com', password: 'password', username: "princess", description: "sickkunt")
 puts "Created the Users"
+file = URI.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfcMkNiLDXVaULaetBzC3xD2HDLaDRmvSKsw&sttps://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%2Fimages%3Fk%3D%2522black%2Bwolf%2522&psig=AOvVaw1492Z1jzi2Y0sHeiaEFDgf&ust=1717854811894000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKjYw4HSyYYDFQAAAAAdAAAAABAE")
+user2.profile_image.attach(io: file, filename: "wolf.png", content_type: "image/png")
+user2.save
 
-member1 = Member.create(user: user1, weekly_points: 0, total_points: 0)
-member2 = Member.create(user: user2, weekly_points: 0, total_points: 0)
-puts "Created the Members"
+user3 = User.create(email: 'thomass2@example.com', password: 'password', username: "ThomASS", description: "greatest human to ever live")
+puts "Created the Users"
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Cima_da_Conegliano%2C_God_the_Father.jpg/640px-Cima_da_Conegliano%2C_God_the_Father.jpg")
+user3.profile_image.attach(io: file, filename: "god.png", content_type: "image/png")
+user3.save
 
-team1 = Team.create(name: 'Team 1', week_number: 1)
-team2 = Team.create(name: 'Team 2', week_number: rand(1..3))
-puts "Created the Team"
 
-20.times do |i|
-  weekly_question = WeeklyQuestion.create!(content: "Sample question #{i+1}")
-  puts "Created the Weekly"
+team1 = Team.create(name: 'Le Wagon', week_number: 1)
+team2 = Team.create(name: 'BreakerBoys', week_number: rand(1..3))
+puts "Created #{Team.count} Teams"
 
-  User.all.each do |user|
-    correct_answer_index = rand(4)
-    puts "fine"
-    wrong_answers = (0..3).to_a - [correct_answer_index]
+member1 = Member.create(user: user1, weekly_points: 0, total_points: 0, team: team1)
+member1 = Member.create(user: user1, weekly_points: 0, total_points: 0, team: team2)
+member2 = Member.create(user: user2, weekly_points: 0, total_points: 0, team: team1)
+puts "Created #{Member.count} Members"
 
-    weekly_answer = WeeklyAnswer.create(
-      content: "Sample answer for question #{i+1}",
-      user: user,
-      correct_answer_index: correct_answer_index,
-      wrong_answers: wrong_answers
-    )
 
-    puts "really"
-    weekly_answer.update(weekly_question_id: weekly_question.id)
-    puts "not really"
-  end
+weekly_question1 = WeeklyQuestion.create!(content: "If you could have any superpower, what would it be?")
+weekly_question2 = WeeklyQuestion.create!(content: "What's your favorite travel destination")
+weekly_question3 = WeeklyQuestion.create!(content: "If you could meet any historical figure, who would it be?")
+puts "Created #{WeeklyQuestion.count} Weeklies"
+
+super_powers = [
+  "Telekinesis",
+  "Invisibility",
+  "Superhuman strength",
+  "Flight",
+  "Time manipulation",
+  "Teleportation",
+  "Mind reading",
+  "Shape-shifting",
+  "Energy manipulation",
+  "Healing factor"
+]
+
+travel_destinations = [
+  "Paris, France",
+  "Tokyo, Japan",
+  "New York City, USA",
+  "Santorini, Greece",
+  "Machu Picchu, Peru",
+  "Sydney, Australia",
+  "Rio de Janeiro, Brazil",
+  "Cape Town, South Africa",
+  "Kyoto, Japan",
+  "Venice, Italy"
+]
+
+historical_figures = [
+  "Leonardo da Vinci",
+  "Cleopatra",
+  "Nelson Mandela",
+  "Queen Elizabeth I",
+  "Albert Einstein",
+  "Mahatma Gandhi",
+  "Joan of Arc",
+  "Winston Churchill",
+  "Marie Curie",
+  "Genghis Khan"
+]
+
+school_names = [
+  "Maplewood High School",
+  "Riverside Academy",
+  "Oakmont Preparatory School",
+  "Crescent Valley Elementary",
+  "Greenfield Middle School",
+  "Willowbrook Elementary",
+  "Sunset Ridge High School",
+  "Pinecrest Academy",
+  "Harmony Hills School",
+  "Briarwood Elementary"
+]
+
+school_subjects = [
+  "Mathematics",
+  "English Language Arts",
+  "Science",
+  "History",
+  "Physical Education",
+  "Art",
+  "Music",
+  "Computer Science",
+  "Foreign Language",
+  "Social Studies"
+]
+
+hobbies = [
+  "Reading",
+  "Painting",
+  "Hiking",
+  "Cooking",
+  "Playing musical instruments",
+  "Photography",
+  "Gardening",
+  "Writing",
+  "Dancing",
+  "Knitting/Crocheting"
+]
+
+teamwork_aspects = [
+  "Communication",
+  "Collaboration",
+  "Trust",
+  "Respect",
+  "Responsibility",
+  "Adaptability",
+  "Leadership",
+  "Problem-solving",
+  "Feedback",
+  "Goal setting"
+]
+
+
+User.all.each do |user|
+
+  # initial question 1
+  initial_answer = InitialAnswer.create(
+    content: school_names.sample,
+    user: user,
+    wrong_answers: "#{school_names.sample}, #{school_names.sample}, #{school_names.sample}"
+  )
+
+  # initial question 2
+  initial_answer = InitialAnswer.create(
+    content: school_names.sample,
+    user: user,
+    wrong_answers: "#{school_names.sample}, #{school_names.sample}, #{school_names.sample}"
+  )
+
+  # initial question 3
+  initial_answer = InitialAnswer.create(
+    content: school_subjects.sample,
+    user: user,
+    wrong_answers: "#{school_subjects.sample}, #{school_subjects.sample}, #{school_subjects.sample}"
+  )
+
+  # initial question 4
+  initial_answer = InitialAnswer.create(
+    content: hobbies.sample,
+    user: user,
+    wrong_answers: "#{hobbies.sample}, #{hobbies.sample}, #{hobbies.sample}"
+  )
+
+  # initial question 5
+  initial_answer = InitialAnswer.create(
+    content: teamwork_aspects.sample,
+    user: user,
+    wrong_answers: "#{teamwork_aspects.sample}, #{teamwork_aspects.sample}, #{teamwork_aspects.sample}"
+  )
+  # weekly question 1
+  weekly_answer = WeeklyAnswer.create(
+    content: super_powers.sample,
+    user: user,
+    wrong_answers: "#{super_powers.sample}, #{super_powers.sample}, #{super_powers.sample}"
+  )
+
+  # weekly question 2
+  weekly_answer = WeeklyAnswer.create(
+    content: travel_destinations.sample,
+    user: user,
+    wrong_answers: "#{travel_destinations.sample}, #{travel_destinations.sample}, #{travel_destinations.sample}"
+  )
+
+  # weekly question 3
+  weekly_answer = WeeklyAnswer.create(
+    content: historical_figures.sample,
+    user: user,
+    wrong_answers: "#{historical_figures.sample}, #{historical_figures.sample}, #{historical_figures.sample}"
+  )
 end
 
 
